d2c3027fee804d2fb6739e5679878c9243b0d6b3
chironito <86181705+chironito@users.noreply.github.com>
2025-07-20 21:13:28 +0530

    Update file-server.py

--- misc_codes/file-server.py
+++ misc_codes/file-server.py
@@ -6,6 +6,12 @@ assert load_dotenv("env.env"), "Environment File not found."
 import os
 import uuid
 import shutil
+from io import BytesIO
+from zipfile import ZipFile
+import mimetypes
+
+STREAM_THRESHOLD = 1024 * 10
+MAX_UPLOAD_SIZE = 1024 * 1024 * 100
 
 class FileList:
     def on_get(self, req, resp):
@@ -21,27 +27,58 @@ class FileList:
         resp.media = {"folders": dirnames, "filenames": filenames}
         return
 
-class GetFile:
+class GetObject:
     def on_get(self, req, resp, path):
         key = req.get_header("Authorization", required=True)
         if hashlib.sha256(key.encode()).hexdigest() != os.environ['file_server_password_sha256']:
             resp.status = falcon.HTTP_401
             resp.text = "Unauthorized"
             return
-        filepath = path
+        stream = req.get_param_as_bool("stream", default=True)
         try:
-            with open(filepath, "rb") as f:
-                content = f.read()
+            filesize = os.path.getsize(path)
+            if filesize >= STREAM_THRESHOLD:
+                resp.stream = open(path, "rb")
+                resp.content_length = filesize
+                resp.stream_len = filesize
+            else:
+                with open(path, "rb") as f:
+                    content = f.read()
+                resp.data = content
+            resp.set_header('Content-Disposition', f'attachment; filename="{os.path.basename(path)}"')
+            resp.content_type = mimetypes.guess_type(str(path))[0] or 'application/octet-stream'
             resp.status = falcon.HTTP_200
-            resp.data = content
-            return
+        except IsADirectoryError:
+            *filepaths, = Path(path).rglob("*")
+            folder_size = sum(map(os.path.getsize, filepaths))
+            if folder_size >= STREAM_THRESHOLD:
+                zip_bytes_io = BytesIO()
+                with ZipFile(zip_bytes_io, "w") as zipfile:
+                    for filepath in filepaths:
+                        zipfile.write(filepath)
+                zip_bytes_io.seek(0, 2)
+                filesize = zip_bytes_io.tell()
+                zip_bytes_io.seek(0)
+                resp.stream = zip_bytes_io
+            else:
+                with ZipFile(f"{path}.zip", "w") as zipfile:
+                    for filepath in filepaths:
+                        zipfile.write(filepath)
+                filesize = os.path.getsize(f"{path}.zip")
+                resp.stream = open(f"{path}.zip", "rb")
+                try:
+                    os.remove(f"{path}.zip")
+                except FileNotFoundError:
+                    pass
+            resp.content_type = 'application/octet-stream'
+            resp.content_length = filesize
+            resp.set_header('Content-Disposition', f'attachment; filename="{os.path.basename(path)}.zip"')
+            resp.stream_len = filesize
+            resp.status = falcon.HTTP_200
         except (FileNotFoundError, TypeError):
             resp.status = falcon.HTTP_404
             resp.text = "File not found"
-            return
-        except IsADirectoryError:
-            resp.status = falcon.HTTP_302
-            resp.text = "Is a Directory"
+        finally:
             return
             
 class CreateObject:
@@ -137,7 +174,12 @@ class DeleteObject:
             return
 
 app = falcon.App()
+
+handler = falcon.media.MultipartFormHandler()
+handler.parse_options.max_body_part_buffer_size = MAX_UPLOAD_SIZE
+app.req_options.media_handlers[falcon.MEDIA_MULTIPART] = handler
+
 app.add_route("/file_list", FileList())
-app.add_route("/get_file/{path:path}", GetFile())
+app.add_route("/get_object/{path:path}", GetObject())
 app.add_route("/create_object", CreateObject())
 app.add_route("/delete_object/{path:path}", DeleteObject())
